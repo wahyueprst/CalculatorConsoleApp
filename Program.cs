08c99c2ea6e74fa79c8e64efaa4557ccc82623b4
wahyueprast <wahyoeko@students.amikom.ac.id>
2021-10-12 17:47:50 +0700

    Console

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CalculatorMachine;
+
+namespace CalculatorConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Calculator Versi Console";
+
+            Console.Write("Inputkan nilai a = ");
+            int a = int.Parse(Console.ReadLine()); // casting
+
+            Console.Write("Inputkan nilai b = ");
+            var b = Convert.ToInt32(Console.ReadLine()); // casting
+
+
+            var cal = new Calculator();
+
+            Console.WriteLine(); // tambahkan baris kosong
+            Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, cal.Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, cal.Pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian: {0} x {1} = {2}", a, b, cal.Perkalian(a, b));
+
+            Console.WriteLine("Hasil Pembagian: {0} / {1} = {2}", a, b, Calculator.Pembagian(a, b));
+            Console.WriteLine("Hasil Perpangkatan: {0} ^ {1} = {2}", a, b, cal.Pangkat(a, b));
+            Console.WriteLine("Hasil Module: {0} % {1} = {2}", a, b, Calculator.Modulo(a, b));
+
+            Console.ReadKey();
+        }
+
+    }
+}
